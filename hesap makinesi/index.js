be79b008bc7ff9ba8f153c8cf0dbbc6816f57795
Aybarssafak <159555967+Aybarssafak@users.noreply.github.com>
2024-07-25 11:41:11 +0300

    Update index.js

--- hesap makinesi/index.js
+++ hesap makinesi/index.js
@@ -5,23 +5,18 @@ document.getElementById("toplam").addEventListener("click", function(){
     document.getElementById("sonuc").innerHTML = toplam;
 })
 /* parseInt kullnmamın sebebi eğer direk olarak sayıyı yazarsam 3 + 3 = 33 çıkıyor o yüzden */
-
 document.getElementById("çıkarma").addEventListener("click", function(){
     let ilkSayı = document.getElementById("ilksayı").value;
     let ikinciSayı = document.getElementById("ikincisayı").value;
     let çıkarma = parseInt(ilkSayı) - parseInt(ikinciSayı);
     document.getElementById("sonuc").innerHTML = çıkarma;
 })
-
 document.getElementById("çarpma").addEventListener("click", function() {
    let ilkSayı = document.getElementById("ilksayı").value;
     let ikinciSayı = document.getElementById("ikincisayı").value;
     let çarpma = parseInt(ilkSayı) * parseInt(ikinciSayı);
     document.getElementById("sonuc").innerHTML = çarpma; 
 })
-    
-
-
 document.getElementById("bölme").addEventListener("click", function(){
     let ilkSayı = document.getElementById("ilksayı").value;
     let ikinciSayı = document.getElementById("ikincisayı").value;
