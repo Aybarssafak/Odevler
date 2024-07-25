369d8a573ae14d492aed403cf4f8c6a01de54b2f
Aybarssafak <159555967+Aybarssafak@users.noreply.github.com>
2024-07-25 11:39:21 +0300

    Add files via upload

---
+++ hesap makinesi/index.js
@@ -0,0 +1,30 @@
+document.getElementById("toplam").addEventListener("click", function(){
+    let ilkSayı = document.getElementById("ilksayı").value;
+    let ikinciSayı = document.getElementById("ikincisayı").value;
+    let toplam = parseInt(ilkSayı) + parseInt(ikinciSayı);
+    document.getElementById("sonuc").innerHTML = toplam;
+})
+/* parseInt kullnmamın sebebi eğer direk olarak sayıyı yazarsam 3 + 3 = 33 çıkıyor o yüzden */
+
+document.getElementById("çıkarma").addEventListener("click", function(){
+    let ilkSayı = document.getElementById("ilksayı").value;
+    let ikinciSayı = document.getElementById("ikincisayı").value;
+    let çıkarma = parseInt(ilkSayı) - parseInt(ikinciSayı);
+    document.getElementById("sonuc").innerHTML = çıkarma;
+})
+
+document.getElementById("çarpma").addEventListener("click", function() {
+   let ilkSayı = document.getElementById("ilksayı").value;
+    let ikinciSayı = document.getElementById("ikincisayı").value;
+    let çarpma = parseInt(ilkSayı) * parseInt(ikinciSayı);
+    document.getElementById("sonuc").innerHTML = çarpma; 
+})
+    
+
+
+document.getElementById("bölme").addEventListener("click", function(){
+    let ilkSayı = document.getElementById("ilksayı").value;
+    let ikinciSayı = document.getElementById("ikincisayı").value;
+    let bölme = parseInt(ilkSayı) / parseInt(ikinciSayı);
+    document.getElementById("sonuc").innerHTML = bölme;
+})
